a58d6bc0ab36a454e13618078ee666410ddcafdf
RFNshare <rfnshare@gmail.com>
2022-06-17 11:42:58 +0600

    Basic task defination using User class

--- locustfile.py
+++ locustfile.py
@@ -1,2 +1,13 @@
 from locust import User, task, between
 
+
+class MyUser(User):
+    wait_time = between(1, 2)
+
+    @task
+    def my_task1(self):
+        print("This is my task 1")
+
+    @task
+    def my_task2(self):
+        print("This is my task 2")
